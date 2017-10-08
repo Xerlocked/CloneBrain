bb5be88f2975cd2b8c8165df7c1f744e2f009372
김근홍 <xhouse.xman@gmail.com>
2017-10-08 21:54:21 +0900

    First Commit

---
+++ CloneBrain/View/Create_Setting.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CloneBrain.View
+{
+    /// <summary>
+    /// Create_Setting.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Create_Setting : Window
+    {
+        public Create_Setting()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CloneBrain/ViewModel/MainWindow.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace CloneBrain.ViewModel
+{
+    public class MainWindow : INotifyPropertyChanged
+    {
+        private int _fileCount;
+
+        private struct _infoFile
+        {
+            string fileName;
+            string fileLink;
+        }
+
+        public int fileCount
+        {
+            get
+            { return _fileCount; }
+            set
+            {
+                _fileCount = value;
+                OnPropertyChanged("Count");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ICommand minusCommand;
+
+        public ICommand MinusCommand
+        {
+            get { return (this.minusCommand) ?? (this.minusCommand = new DelegateCommand(Minus)); }
+        }
+
+        private void Minus()
+        {
+            _fileCount -= 1;
+        }
+
+        private ICommand plusCommand;
+
+        public ICommand PlusCommand
+        {
+            get { return (this.plusCommand) ?? (this.plusCommand = new DelegateCommand(Plus)); }
+        }
+
+        private void Plus()
+        {
+            _fileCount += 1;
+        }
+    }
+
+    #region DelegateCommand Class
+    public class DelegateCommand : ICommand
+    {
+
+        private readonly Func<bool> canExecute;
+        private readonly Action execute;
+
+        /// <summary>
+        /// Initializes a new instance of the DelegateCommand class.
+        /// </summary>
+        /// <param name="execute">indicate an execute function</param>
+        public DelegateCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DelegateCommand class.
+        /// </summary>
+        /// <param name="execute">execute function </param>
+        /// <param name="canExecute">can execute function</param>
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        /// <summary>
+        /// can executes event handler
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// implement of icommand can execute method
+        /// </summary>
+        /// <param name="o">parameter by default of icomand interface</param>
+        /// <returns>can execute or not</returns>
+        public bool CanExecute(object o)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        /// <summary>
+        /// implement of icommand interface execute method
+        /// </summary>
+        /// <param name="o">parameter by default of icomand interface</param>
+        public void Execute(object o)
+        {
+            this.execute();
+        }
+
+        /// <summary>
+        /// raise ca excute changed when property changed
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            if (this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+    #endregion
+}
